f875f9e948dbb21f4ce2a508b513af3656dece05
Robiul Hossain <robiulhossain0110@gmail.com>
2025-05-02 19:58:16 +0600

    i created php site

--- dev2.php
+++ dev2.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 
 <!DOCTYPE html>
@@ -6,11 +6,10 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>php_check_syntax</title>
+  <title>Document</title>
 </head>
 <body>
   
 </body>
 </html>
-
 >
